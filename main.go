d2ee578e6669235105b7fee62343558f58c917ae
Elias Feijó de Almeida Pereira Lima <eliasfeijo.dev@gmail.com>
2023-12-30 06:46:33 -0300

    feat: simple http server

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Printf("Hello world")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome!"))
+	})
+	http.ListenAndServe(":8080", r)
 }
