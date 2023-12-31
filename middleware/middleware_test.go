3c80e7100944494ee4b5a07dce52a6500d61e07e
Elias Feijó de Almeida Pereira Lima <eliasfeijo.dev@gmail.com>
2023-12-31 18:28:23 -0300

    test: middleware serve http success

---
+++ middleware/middleware_test.go
@@ -0,0 +1,65 @@
+package middleware
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/eliasfeijo/go-rate-limiter/config"
+	"github.com/go-chi/chi/v5"
+)
+
+func testRequest(t *testing.T, server *httptest.Server) (*http.Response, string) {
+	// Create a mock HTTP request
+	req, err := http.NewRequest("GET", server.URL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+		return nil, ""
+	}
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+		return nil, ""
+	}
+	defer resp.Body.Close()
+
+	return resp, string(respBody)
+}
+
+func TestRateLimiterMiddleware_ServeHTTP_Success(t *testing.T) {
+
+	cfg := &config.RateLimiterConfig{
+		IpAddressMaxRequests:    3,
+		IpAddressLimitInSeconds: 1,
+		IpAddressBlockInSeconds: 5,
+		MapTokenConfig:          nil,
+		TokensHeaderKey:         "API_KEY",
+		StoreStrategy:           "in_memory",
+		RedisConfig:             config.RedisConfig{},
+	}
+
+	// Create a new instance of the RateLimiterMiddleware
+	middleware := NewRateLimitMiddleware(cfg)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Handler)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Request accepted"))
+	})
+
+	server := httptest.NewServer(r)
+	defer server.Close()
+
+	// Create a mock HTTP request
+	if resp, body := testRequest(t, server); body != "root" && resp.StatusCode != 200 {
+		t.Fatalf(body)
+	}
+}
